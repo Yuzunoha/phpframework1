6790e22d413148e9b4dce88d2834ef87513f60e2
Yuzunoha <studysteadystandby@gmail.com>
2020-02-18 16:15:09 +0900

    できた。名前空間が重要だ

--- controllers/Api.php
+++ controllers/Api.php
@@ -1,9 +1,16 @@
 <?php
 
+namespace controllers;
+
 class Api
 {
     public function index(): array
     {
         return array('a' => 1, 'b' => 2, 'c' => 3, 'd' => 4, 'e' => 5);
+    }
+
+    public function test(): array
+    {
+        return array('msg' => "test");
     }
 }
--- index.php
+++ index.php
@@ -1,10 +1,25 @@
 <?php
-include './controllers/Api.php';
-
-$api = new Api();
-
-$obj = $api->index();
-
-$json = json_encode($obj);
-
-echo $json;
+if (empty($_SERVER['PATH_INFO'])) {
+    exit;
+}
+//スラッシュで区切られたurlを取得します
+$parse = explode('/', $_SERVER['PATH_INFO']);
+$call = "";
+foreach ($parse as $value) {
+    if ($value !== "") {
+        $call = $value;
+        break;
+    }
+}
+//ApiControllerをインクルードし、JSONを返します
+if (file_exists('./controllers/' . $call . '.php')) {
+    include './controllers/' . $call . '.php';
+    //$call名のcontrollerをインスタンス化します
+    $className = "controllers\\" . $call;
+    $obj = new $className();
+    //controllerのindexメソッドを呼びます
+    $response = json_encode($obj->index());
+    //ヘッダーを指定してJSONを出力
+    header("Content-Type: application/json; charset=utf-8");
+    echo $response;
+}
